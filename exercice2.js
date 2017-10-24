5e21f851566000f19f70b0a6023e65f70bfd6716
AndreVictor60 <andre.victor60@free.fr>
2017-10-24 09:19:55 +0200

    Exercice 2 JavaScript

---
+++ exercice2.js
@@ -0,0 +1,8 @@
+// Variables
+var nom= 'Nom : Doe\n';
+var prenom= 'Prénom : John\n';
+var ville= 'Ville : New-York'
+//Concaténation des variables
+result = nom + prenom + ville;
+// Afficher le résultalt
+alert(result);
